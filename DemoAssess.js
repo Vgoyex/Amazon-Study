7374b927a3cc1f095b51afb1a59d006b4286327a
Vgoyex <fgoya70@gmail.com>
2023-10-30 12:30:11 -0300

    DemoAssessment att

--- DemoAssess.js
+++ DemoAssess.js
@@ -6,43 +6,47 @@
 1,2, 6,5,4,3, 7,8,9    [3,6]
 */
 function performOperations(arr, operations) {
-    // Write your code here
-    if(!arr || !operations){
-        return `Invalid argument arr: ${arr} operations: ${operations}`;
+  // Write your code here
+  if (!arr || !operations) {
+    return `Invalid argument arr: ${arr} operations: ${operations}`;
+  }
+  let newArr = [];
+  let replacedArr = [];
+  //Just for operations array
+  for (let i = 0; i < operations.length; i++) {
+    // let j = i - 1;
+    let tt = [];
+    let arrMiddle = [];
+    let arrLeft = [];
+    let arrRight = [];
+    for (let j = 0; j < operations.length; j++) {
+      let opi = operations[j];
+      if (i == j) {
+        arrLeft = [arr.slice(arr.length, opi[i - 1])];
+        arrMiddle = [arr.slice(opi[i], opi[i + 1])];
+        arrRight = [arr.slice(opi[i + 1], opi[arr.length])];
+        tt = arrMiddle[0].reverse();
+      } else {
+        arrLeft = [arr.slice(arr[-2], arr.indexOf(opi[j - 1]))];
+        arrMiddle = [arr.slice(arr.indexOf(opi[j - 1]), opi[j])];
+        arrRight = [arr.slice(opi[j], opi[arr.length])];
+        tt = arrMiddle[0].reverse();
+      }
+      replacedArr = replacedArr.concat(arrLeft[0], tt, arrRight[0]);
+      newArr = newArr.concat([replacedArr]);
+      replacedArr = [];
     }
-
-    let newArr = [];
-    let replacedArr = [];
-    //Just for operations array
-    for(let i = 0; i < operations.length ;i++){
-            let j = i - 1;
-            let opi = operations[i];
-            let tt = [];
-            let arrMiddle = [];
-            let arrLeft = [];
-            let arrRight = [];
-            if(opi[i]==0){
-                // arrLeft = [arr.slice(arr.length,opi[i-1])];
-                // arrRight = [arr.slice(opi[i+1],opi[arr.length])];
-                // arrMiddle = [arr.slice(opi[i],opi[i+1])];  
-                arrLeft = [arr.slice(arr.length,opi[i-1])];
-                arrRight = [arr.slice(opi[i+1],opi[arr.length])];
-                arrMiddle = [arr.slice(opi[i],opi[i+1])];  
-                tt = arrMiddle[0].reverse();
-            }else{
-                // arrLeft = [arr.slice(arr[-1],arr.indexOf(opi[i-1]))];
-                // arrRight = [arr.slice(opi[i],opi[arr.length])];
-                // arrMiddle = [arr.slice(arr.indexOf(opi[i-1]),opi[i])];
-                arrLeft = [arr.slice(arr[-2],arr.indexOf(opi[i-1]))];
-                arrRight = [arr.slice(opi[i],opi[arr.length])];
-                arrMiddle = [arr.slice(arr.indexOf(opi[i-1]),opi[i])];
-                tt = arrMiddle[0].reverse();
-            }
-            replacedArr = replacedArr.concat(arrLeft[0],tt,arrRight[0]);
-            newArr = newArr.concat([replacedArr]);
-            replacedArr = [];
-
-    }
-    return [newArr];
+  }
+  return [newArr];
 }
-performOperations([1,2,3,4,5,6,7,8,9],[[0,9],[3,6],[4,5],[2,3]])
+console.log(
+  performOperations(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [
+      [0, 9],
+      [3, 6],
+      [4, 5],
+      [2, 3],
+    ]
+  )
+);
